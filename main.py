c9573f1801836b2c43757dbae1e9c22ea2811307
Frederik <s204213@win.dtu.dk>
2025-11-04 17:37:09 +0100

    add example in main

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello World!")
-
+from utility.file_utils import get_csv_dataframe
+from utility.meta_data_utils import calculate_metadata
 
 if __name__ == "__main__":
-    main()
+    # Example usage
+    df = get_csv_dataframe("data/AI_Human.csv")
+    
+    #Sample 1471 is AI generated
+    n = 1471
+    sample_text = df["text"][n]
+    label = df['generated'][n]
+    metadata = calculate_metadata(sample_text)
+    
+    print("Text sample:\n", sample_text)
+
+    print("\nCalculated Metadata:")
+    for key, value in metadata.items():
+        print(f"{key}: {value}")
+
+    print("\nAI generated: ", label)
+        
