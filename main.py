b97a60acf579538ae010212f4364289ca8481a57
Frederik <s204213@win.dtu.dk>
2025-11-05 14:38:18 +0100

    run make check

--- main.py
+++ main.py
@@ -4,13 +4,13 @@ from utility.meta_data_utils import calculate_metadata
 if __name__ == "__main__":
     # Example usage
     df = get_csv_dataframe("data/AI_Human.csv")
-    
-    #Sample 1471 is AI generated
+
+    # Sample 1471 is AI generated
     n = 1471
     sample_text = df["text"][n]
-    label = df['generated'][n]
+    label = df["generated"][n]
     metadata = calculate_metadata(sample_text)
-    
+
     print("Text sample:\n", sample_text)
 
     print("\nCalculated Metadata:")
@@ -18,4 +18,3 @@ if __name__ == "__main__":
         print(f"{key}: {value}")
 
     print("\nAI generated: ", label)
-        
